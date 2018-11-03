1fd3a2abfe38b6f2c7d7b02639c81c1f44faff8e
oalfonso <oalfonso@yahoo.com>
2018-11-03 21:35:25 +0000

    Add positional parameters

--- csv2json.py
+++ csv2json.py
@@ -1,8 +1,14 @@
 import csv
 import json
+import argparse
 
-with open("testdata/23_Time_Card.csv",  encoding='utf-8-sig') as f:
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="csv input file")
+parser.add_argument("output", help="json output file")
+args=parser.parse_args()
+
+with open(args.input,  encoding='utf-8-sig') as f:
     reader = csv.DictReader(f, delimiter=";")
     data = list(reader)
-    with open('target/data.json', 'w') as outfile:
+    with open(args.output, 'w') as outfile:
         json.dump(data, outfile, indent = 4)
