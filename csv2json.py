0bf4f1b00733924509ee4aacce3b895287dd7dd0
oalfonso <oalfonso@yahoo.com>
2018-11-04 21:50:57 +0000

    New parameter, the delimiter

--- csv2json.py
+++ csv2json.py
@@ -2,13 +2,14 @@ import csv
 import json
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input", help="csv input file")
-parser.add_argument("output", help="json output file")
+parser = argparse.ArgumentParser(description='Simple CSV to JSON converter')
+parser.add_argument("-i", "--input", dest="input", help="csv input file", required=True)
+parser.add_argument("-o", "--output", dest="output", help="json output file", required=True)
+parser.add_argument("-d", "--delimiter", dest="delimiter", help="Delimiter char", default=",")
 args=parser.parse_args()
 
 with open(args.input,  encoding='utf-8-sig') as f:
-    reader = csv.DictReader(f, delimiter=";")
+    reader = csv.DictReader(f, delimiter=args.delimiter)
     data = list(reader)
     with open(args.output, 'w') as outfile:
         json.dump(data, outfile, indent = 4)
